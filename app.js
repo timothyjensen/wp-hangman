05706959cf7ac2aa68c15f17d3730f2bfad3d72b
Calvin Koepke <hello@calvinkoepke.com>
2017-07-07 14:27:44 -0600

    Initial.

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(w) {
+
+	// First off, come up with a list of variables that you think we might need to hold data and state. First one is free, and will be used to hold available guesses.
+
+	var guesses,
+		characterOptions,
+		answer;
+
+})(window);
